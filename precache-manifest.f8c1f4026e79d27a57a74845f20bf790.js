f73ed73cfe4363f11fd54c68651be270fa5bb6ac
yiyeop <syl0326@gmail.com>
2019-03-21 15:38:37 +0900

    Updates

---
+++ precache-manifest.f8c1f4026e79d27a57a74845f20bf790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9ccda40f1d1b4d1ff07",
+    "url": "/ideal/static/js/runtime~main.06b446f7.js"
+  },
+  {
+    "revision": "a3c4a44614a1ccb1f814",
+    "url": "/ideal/static/js/main.859f8475.chunk.js"
+  },
+  {
+    "revision": "ba7e9be2e8c1653e47c1",
+    "url": "/ideal/static/js/2.a03e2c82.chunk.js"
+  },
+  {
+    "revision": "a3c4a44614a1ccb1f814",
+    "url": "/ideal/static/css/main.4d98d6a7.chunk.css"
+  },
+  {
+    "revision": "eeb3088da34ea4b7bcf039dbbdf98330",
+    "url": "/ideal/index.html"
+  }
+];
